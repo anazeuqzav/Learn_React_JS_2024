689b501985dafbcbbec863fec3fa7056c040acde
anazeuqzav <anazeuqzav@gmail.com>
2025-01-15 20:32:43 +0100

    first commit

---
+++ react_basics/components/Main.jsx
@@ -0,0 +1,15 @@
+function Main(){
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="facts-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Wlake</li>
+                <li>Has well over 200K starts on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+export default Main;
