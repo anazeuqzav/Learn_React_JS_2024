6934f4ee8f84dacce7c0a31fb70a6fdcd612e1cf
anazeuqzav <anazeuqzav@gmail.com>
2025-02-13 19:33:20 +0100

    useState and conditional render

---
+++ chef_claude_project/components/Header.jsx
@@ -0,0 +1,13 @@
+
+import chefClaudeLogo from "/images/chef-claude-icon.png";
+
+const Header = () => {
+    return (
+    <header>
+        <img src= {chefClaudeLogo} alt="chef icon"/>
+        <h1>Chef Claude</h1>  
+    </header>
+    )
+}
+
+export default Header;
