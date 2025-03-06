b57a4f34dbe3ad2a66b990bd44f57cd085a54c2a
anazeuqzav <anazeuqzav@gmail.com>
2025-03-06 15:57:58 +0100

    meme generator completed

--- meme-generator-project/src/components/Main.jsx
+++ meme-generator-project/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Main = () => {
     const [meme, setMeme] = useState({
@@ -6,6 +6,24 @@ const Main = () => {
         bottomText: "Walk into Mordor", 
         imageURL: "http://i.imgflip.com/1bij.jpg"
     })
+    const [allMemes, setAllMemes] = useState([])
+
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageURL: newMemeUrl,
+        }))
+    }
+
 
     function handleChange (event) {
         const {value, name} = event.currentTarget
@@ -37,7 +55,7 @@ const Main = () => {
                         value = {meme.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick = {getMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src= {meme.imageURL} />
